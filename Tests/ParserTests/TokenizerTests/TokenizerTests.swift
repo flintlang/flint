152ea4c2389a83c02ac68f01d89622d86a6143ff
Franklin Schrans <f.schrans@me.com>
2017-12-20 18:22:39 +0100

    Fix test

--- Tests/ParserTests/TokenizerTests/TokenizerTests.swift
+++ Tests/ParserTests/TokenizerTests/TokenizerTests.swift
@@ -19,13 +19,13 @@ class TokenizerTests: XCTestCase {
       }
       
       Wallet :: (any) {
-        public mutating func deposit(Ether ether) {
+        public mutating func deposit(ether: Ether) {
           state.contents = state.contents + money
         }
       }
       
       Wallet :: (owner) {
-        public mutating func withdraw(Ether ether) {
+        public mutating func withdraw(ether: Ether) {
           state.contents = state.contents - money
         }
       
@@ -41,13 +41,13 @@ class TokenizerTests: XCTestCase {
             .identifier("Address"), .var, .identifier("contents"), .punctuation(.colon), .identifier("Ether"),
             .punctuation(.closeBrace), .identifier("Wallet"), .punctuation(.doubleColon), .punctuation(.openBracket),
             .identifier("any"), .punctuation(.closeBracket), .punctuation(.openBrace), .public, .mutating, .func,
-            .identifier("deposit"), .punctuation(.openBracket), .identifier("Ether"), .identifier("ether"),
+            .identifier("deposit"), .punctuation(.openBracket), .identifier("ether"), .punctuation(.colon), .identifier("Ether"),
             .punctuation(.closeBracket), .punctuation(.openBrace), .identifier("state"), .binaryOperator(.dot),
             .identifier("contents"), .binaryOperator(.equal), .identifier("state"), .binaryOperator(.dot),
             .identifier("contents"), .binaryOperator(.plus), .identifier("money"), .punctuation(.closeBrace),
             .punctuation(.closeBrace), .identifier("Wallet"), .punctuation(.doubleColon), .punctuation(.openBracket),
             .identifier("owner"), .punctuation(.closeBracket), .punctuation(.openBrace), .public, .mutating, .func,
-            .identifier("withdraw"), .punctuation(.openBracket), .identifier("Ether"), .identifier("ether"),
+            .identifier("withdraw"), .punctuation(.openBracket), .identifier("ether"), .punctuation(.colon), .identifier("Ether"),
             .punctuation(.closeBracket), .punctuation(.openBrace), .identifier("state"), .binaryOperator(.dot),
             .identifier("contents"), .binaryOperator(.equal), .identifier("state"), .binaryOperator(.dot),
             .identifier("contents"), .binaryOperator(.minus), .identifier("money"), .punctuation(.closeBrace),
