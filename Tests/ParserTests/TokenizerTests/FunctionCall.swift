56ca9819033875c85bc568a7f8e507ab288a3a3c
Franklin Schrans <f.schrans@me.com>
2017-12-22 11:24:29 +0100

    Update tokenizer test infrastructure

---
+++ Tests/ParserTests/TokenizerTests/FunctionCall.swift
@@ -0,0 +1,15 @@
+//
+//  FunctionCall.swift
+//  Parser
+//
+//  Created by Franklin Schrans on 12/22/17.
+//
+
+import XCTest
+import Parser
+
+class FunctionCall: XCTestCase {
+   func test() {
+      
+   }
+}
---
+++ Tests/ParserTests/TokenizerTests/TokenizerTest.swift
@@ -0,0 +1,30 @@
+//
+//  TokenizerTest.swift
+//  ParserTests
+//
+//  Created by Franklin Schrans on 12/22/17.
+//
+
+import XCTest
+import Parser
+
+protocol TokenizerTest {
+   var sourceCode: String { get }
+   var expectedTokens: [Token] { get }
+}
+
+extension TokenizerTest {
+   func test() {
+      let file = URL(fileURLWithPath: NSTemporaryDirectory() + UUID().uuidString)
+      SourceFile(contents: sourceCode).write(to: file)
+      XCTAssertEqual(Tokenizer(inputFile: file).tokenize(), expectedTokens)
+   }
+}
+
+fileprivate struct SourceFile {
+   var contents: String
+
+   func write(to location: URL) {
+      try! contents.data(using: .utf8)?.write(to: location)
+   }
+}
