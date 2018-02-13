0bec3461517b3408be9f00298e5fa6435b4fb9ed
Franklin Schrans <f.schrans@me.com>
2018-02-13 14:25:40 +0000

    Remove unused function freshKeyOffset

--- Sources/IRGen/ContractStorage.swift
+++ Sources/IRGen/ContractStorage.swift
@@ -23,13 +23,4 @@ struct ContractStorage {
   mutating func allocate(_ numEntries: Int, for property: String) {
     for _ in (1..<numEntries) { nextIndex() }
   }
-
-  // Dictionaries
-  private var dictionaryNumKeys = [String: Int]()
-
-  mutating func freshKeyOffset(forDictionary dictionary: String) -> Int {
-    let offset = dictionaryNumKeys[dictionary, default: 0]
-    dictionaryNumKeys[dictionary, default: 0] += 1
-    return offset
-  }
 }
