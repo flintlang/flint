02a55d90ae67448c23859a23badd6c0202073d24
Franklin Schrans <f.schrans@me.com>
2018-05-09 14:33:11 +0100

    Make diagnostic messages more consistent

--- Sources/SemanticAnalyzer/TypeError.swift
+++ Sources/SemanticAnalyzer/TypeError.swift
@@ -9,14 +9,14 @@ import AST
 
 extension Diagnostic {
   static func incompatibleReturnType(actualType: Type.RawType, expectedType: Type.RawType, expression: Expression) -> Diagnostic {
-    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Cannot convert expression of type \(actualType.name) to expected return type \(expectedType.name)")
+    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Cannot convert expression of type '\(actualType.name)' to expected return type '\(expectedType.name)'")
   }
 
   static func incompatibleAssignment(lhsType: Type.RawType, rhsType: Type.RawType, expression: Expression) -> Diagnostic {
-    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Incompatible assignment between values of type \(lhsType.name) and \(rhsType.name)")
+    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Incompatible assignment between values of type '\(lhsType.name)' and '\(rhsType.name)'")
   }
 
   static func incompatibleArgumentType(actualType: Type.RawType, expectedType: Type.RawType, expression: Expression) -> Diagnostic {
-    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Cannot convert expression of type \(actualType.name) to expected argument type \(expectedType.name)")
+    return Diagnostic(severity: .error, sourceLocation: expression.sourceLocation, message: "Cannot convert expression of type '\(actualType.name)' to expected argument type '\(expectedType.name)'")
   }
 }
