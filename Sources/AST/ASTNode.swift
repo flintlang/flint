44033d20b0788a9a7fbc436ed48cff69135846a2
Daniel Hails <daniel@hails.info>
2018-08-21 17:31:13 +0100

    Add ASTNode

---
+++ Sources/AST/ASTNode.swift
@@ -0,0 +1,11 @@
+//
+//  ASTNode.swift
+//  AST
+//
+//  Created by Hails, Daniel J R on 21/08/2018.
+//
+
+/// An node of the AST defined by a position in the source file.
+public protocol ASTNode: SourceEntity, CustomStringConvertible {
+  var sourceLocation: SourceLocation { get }
+}
