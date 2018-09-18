flintlang/flint

commit 93eb9d9d48fcf5c2a13fb93cd68ec85d55380368

Trait specification contracts (#360)
* Support declaration of a contract conforming to a trait

* Add syntax to grammar

Sources/AST/ASTDumper.swift
@@ -65,6 +65,13 @@ public class ASTDumper {
     writeNode("ContractDeclaration") {
       self.dump(contractDeclaration.contractToken)
       self.dump(contractDeclaration.identifier)
+      if !contractDeclaration.conformances.isEmpty {
+        self.writeNode("Conforms to") {
+          for traitIdentifier in contractDeclaration.conformances {
+            self.dump(traitIdentifier)
+          }
+        }
+      }
       if !contractDeclaration.states.isEmpty {
         self.dump(contractDeclaration.states)
       }

Sources/Parser/Parser+Declaration.swift
@@ -47,17 +47,19 @@ extension Parser {
   func parseContractDeclaration() throws -> ContractDeclaration {
     let contractToken = try consume(.contract, or: .badTopLevelDeclaration(at: latestSource))
     let identifier = try parseIdentifier()
-    let states: [TypeState]
+    var states: [TypeState] = []
+    var conformances: [Identifier] = []
+    if currentToken?.kind == .punctuation(.colon) {
+      conformances = try parseConformances()
+    }
     if currentToken?.kind == .punctuation(.openBracket) {
       states = try parseTypeStateGroup()
-    } else {
-      states = []
     }
     try consume(.punctuation(.openBrace), or: .leftBraceExpected(in: "contract declaration", at: latestSource))
     let members = try parseContractMembers(enclosingType: identifier.name)
     try consume(.punctuation(.closeBrace), or: .rightBraceExpected(in: "contract declaration", at: latestSource))
 
-    return ContractDeclaration(contractToken: contractToken, identifier: identifier, states: states, members: members)
+    return ContractDeclaration(contractToken: contractToken, identifier: identifier, conformances: conformances, states: states, members: members)
   }
 
   func parseStructDeclaration() throws -> StructDeclaration {

Sources/Parser/ParserError.swift
@@ -54,6 +54,9 @@ extension Diagnostic {
   static func expectedBehaviourSeparator(at sourceLocation: SourceLocation) -> Diagnostic {
     return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected behaviour separator")
   }
+  static func expectedConformance(at sourceLocation: SourceLocation) -> Diagnostic {
+    return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected conformance")
+  }
 
   // MARK: Statement
   static func expectedStatement(at sourceLocation: SourceLocation) -> Diagnostic {

Sources/AST/Declaration/ContractDeclaration.swift
@@ -13,6 +13,7 @@ public struct ContractDeclaration: ASTNode {
 
   public var contractToken: Token
   public var identifier: Identifier
+  public var conformances: [Identifier]
   public var states: [TypeState]
   public var members: [ContractMember]
 
@@ -46,17 +47,19 @@ public struct ContractDeclaration: ASTNode {
     return EnumDeclaration(enumToken: enumToken, identifier: stateEnumIdentifier, type: intType, cases: cases)
   }
 
-  public init(contractToken: Token, identifier: Identifier, states: [TypeState], members: [ContractMember]) {
+  public init(contractToken: Token, identifier: Identifier, conformances: [Identifier], states: [TypeState], members: [ContractMember]) {
     self.identifier = identifier
     self.members = members
+    self.conformances = conformances
     self.states = states
     self.contractToken = contractToken
   }
 
   // MARK: - ASTNode
   public var description: String {
-    let stateText = states.map({ $0.description }).joined(separator: " ")
-    let headText = "contract \(identifier) \(stateText)"
+    let conformsText = conformances.map ({ $0.description }).joined(separator: ", ")
+    let stateText = states.map({ $0.description }).joined(separator: ", ")
+    let headText = "contract \(identifier): \(conformsText) \(stateText)"
     let memberText = members.map({ $0.description }).joined(separator: "\n")
     return "\(headText) {\(memberText)}"
   }

Sources/Parser/Parser+Components.swift
@@ -21,17 +21,18 @@ extension Parser {
 
   func parseIdentifierGroup() throws -> (identifiers: [Identifier], closeBracketToken: Token) {
     try consume(.punctuation(.openBracket), or: .badDeclaration(at: latestSource))
-    let identifiers = try parseIdentifierList()
+    guard let closingIndex = indexOfFirstAtCurrentDepth([.punctuation(.closeBracket)]) else {
+      throw raise(.expectedCloseParen(at: latestSource))
+    }
+    let identifiers = try parseIdentifierList(upTo: closingIndex)
     let closeBracketToken = try consume(.punctuation(.closeBracket), or: .expectedCloseParen(at: latestSource))
 
     return (identifiers, closeBracketToken)
   }
 
-  func parseIdentifierList() throws -> [Identifier] {
+  func parseIdentifierList(upTo closingIndex: Int) throws -> [Identifier] {
     var identifiers = [Identifier]()
-    guard let closingIndex = indexOfFirstAtCurrentDepth([.punctuation(.closeBracket)]) else {
-      return []
-    }
+
     while currentIndex < closingIndex {
       identifiers.append(try parseIdentifier())
       if currentIndex < closingIndex {
@@ -155,6 +156,15 @@ extension Parser {
     }
 
     return (callerCapabilities, closeBracketToken)
+  }
+
+  // MARK: Conformances
+  func parseConformances() throws -> [Identifier] {
+    try consume(.punctuation(.colon), or: .expectedConformance(at: latestSource))
+    guard let endOfConformances = indexOfFirstAtCurrentDepth([.punctuation(.openBracket), .newline, .punctuation(.openBrace)]) else {
+      throw raise(.expectedConformance(at: latestSource))
+    }
+    return try parseIdentifierList(upTo: endOfConformances)
   }
 
   // MARK: Type State

Sources/Parser/Parser.swift
@@ -55,6 +55,7 @@ public class Parser {
         switch tld {
         case .contractDeclaration(let contract):
           environment.addContract(contract)
+          // TODO: Add conformances here 
           if contract.isStateful {
             environment.addEnum(contract.stateEnum)
           }

Tests/ParserTests/conformance.flint
@@ -1,0 +1,44 @@
+// RUN: %flintc %s --dump-ast | %FileCheck %s --prefix CHECK-AST
+
+// CHECK-AST: ContractDeclaration
+// CHECK-AST:  identifier "C2"
+// CHECK-AST:  Conforms to
+// CHECK-AST:    "Ownable"
+// CHECK-AST:    "Burnable"
+// CHECK-AST:  States
+// CHECK-AST:    TypeState
+// CHECK-AST:      "State1"
+// CHECK-AST:    TypeState
+// CHECK-AST:      "State2"
+// CHECK-AST:    TypeState
+// CHECK-AST:      "State3"
+contract C2: Ownable, Burnable (State1, State2, State3) {}
+
+C2 @(any) :: (any) {
+  public init(){
+    become State1
+  }
+}
+
+// CHECK-AST: ContractDeclaration
+// CHECK-AST:  identifier "C3"
+// CHECK-AST:  Conforms to
+// CHECK-AST:    "Ownable"
+// CHECK-AST:    "Burnable"
+
+contract C3: Ownable, Burnable {}
+
+C3 :: (any) {
+  public init(){}
+}
+
+// CHECK-AST: ContractDeclaration
+// CHECK-AST:  identifier "C4"
+// CHECK-AST:  Conforms to
+// CHECK-AST:    "XSad999sads"
+
+contract C4: XSad999sads {}
+
+C4 :: (any) {
+  public init(){}
+}

docs/grammar.abnf
@@ -9,7 +9,7 @@ topLevelDeclaration = contractDeclaration
                         / enumDeclaration;
 
 ; CONTRACTS
-contractDeclaration = %s"contract" SP identifier SP [identifierGroup] SP "{" *(WSP variableDeclaration CRLF) "}";
+contractDeclaration = %s"contract" SP identifier [":" WSP identifierList ] SP [identifierGroup] SP "{" *(WSP variableDeclaration CRLF) "}";
 
 ; VARIABLES
 variableDeclaration = [*(modifier SP)] SP (%s"var" / %s"let") SP identifier typeAnnotation [WSP "=" WSP expression];
@@ -55,10 +55,11 @@ contractBehaviourMember = functionDeclaration
                             / fallbackDeclaration;
 
 ; ACCESS GROUPS
-stateGroup               = "@" identifierGroup;
+stateGroup              = "@" identifierGroup;
 callerCapabilityBinding = identifier WSP "<-";
 callerCapabilityGroup   = identifierGroup;
-identifierGroup           = "(" identifier *("," WSP identifier) ")";
+identifierGroup         = "(" identifierList ")";
+identifierList          = identifier *("," WSP identifier)
 
 ; FUNCTIONS + INITIALIZER + FALLBACK
 functionDeclaration    = functionHead SP identifier parameterList [returnType] codeBlock;

commit a6c502b1e548a4235fc686f9ba432a6f53bc12a9

Fix build broken by #360

Sources/AST/ASTDumper.swift
@@ -118,11 +118,7 @@ public class ASTDumper {
     writeNode("StructDeclaration") {
       self.dump(structDeclaration.identifier)
       if !structDeclaration.conformances.isEmpty {
-        self.writeNode("Conforms to") {
-          for traitIdentifier in structDeclaration.conformances {
-            self.dump(traitIdentifier)
-          }
-        }
+        self.dump(structDeclaration.conformances)
       }
       for member in structDeclaration.members {
         self.dump(member)

Sources/AST/Declaration/StructDeclaration.swift
@@ -11,7 +11,7 @@ import Lexer
 public struct StructDeclaration: ASTNode {
   public var structToken: Token
   public var identifier: Identifier
-  public var conformances: [Identifier]
+  public var conformances: [Conformance]
   public var members: [StructMember]
 
   public var variableDeclarations: [VariableDeclaration] {
@@ -59,7 +59,7 @@ public struct StructDeclaration: ASTNode {
     return unassignedProperties.count == 0
   }
 
-  public init(structToken: Token, identifier: Identifier, conformances: [Identifier], members: [StructMember]) {
+  public init(structToken: Token, identifier: Identifier, conformances: [Conformance], members: [StructMember]) {
     self.structToken = structToken
     self.identifier = identifier
     self.members = members

Sources/Parser/Parser+Declaration.swift
@@ -70,7 +70,7 @@ extension Parser {
   func parseStructDeclaration() throws -> StructDeclaration {
     let structToken = try consume(.struct, or: .badTopLevelDeclaration(at: latestSource))
     let identifier = try parseIdentifier()
-    var conformances: [Identifier] = []
+    var conformances: [Conformance] = []
     if currentToken?.kind == .punctuation(.colon) {
       conformances = try parseConformances()
     }

Sources/Parser/ParserError.swift
@@ -54,9 +54,6 @@ extension Diagnostic {
   static func expectedBehaviourSeparator(at sourceLocation: SourceLocation) -> Diagnostic {
     return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected behaviour separator")
   }
-  static func expectedConformance(at sourceLocation: SourceLocation) -> Diagnostic {
-    return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected conformance")
-  }
 
   // MARK: Statement
   static func expectedStatement(at sourceLocation: SourceLocation) -> Diagnostic {
@@ -139,7 +136,7 @@ extension Diagnostic {
   static func expectedEndAfterInout(at sourceLocation: SourceLocation) -> Diagnostic {
     return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected ',' or ')' after inout identifier")
   }
-  
+
   // MARK: Generics
   static func expectedRightChevron(in node: String, at sourceLocation: SourceLocation) -> Diagnostic {
     return Diagnostic(severity: .error, sourceLocation: sourceLocation, message: "Expected '>' to complete \(node)")