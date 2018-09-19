ccc11e83c1cdd3076db1ee75f1360fcb01b24efa
Alexander Harkness <me@bearbin.net>
2018-09-19 14:49:50 +0100

    Remove unneccessary function added in IRStruct

--- Sources/IRGen/IRStruct.swift
+++ Sources/IRGen/IRStruct.swift
@@ -16,14 +16,8 @@ public struct IRStruct {
   var structDeclaration: StructDeclaration
   var environment: Environment
 
-  func constructParameter(name: String, type: RawType, sourceLocation: SourceLocation) -> Parameter {
-    let identifier = Identifier(identifierToken: Token(kind: .identifier(name), sourceLocation: sourceLocation))
-    return Parameter(identifier: identifier, type: Type(inferredType: type, identifier: identifier), implicitToken: nil)
-  }
-
   func rendered() -> String {
     // At this point, the initializers and conforming functions have been converted to functions.
-
     let functionsCode = structDeclaration.functionDeclarations.compactMap { functionDeclaration in
       return IRFunction(functionDeclaration: functionDeclaration, typeIdentifier: structDeclaration.identifier, environment: environment).rendered()
     }.joined(separator: "\n\n")
