51554f9ec7db170720784fd814cd7568a76bb14a
Aurel <Aurel300@users.noreply.github.com>
2018-10-23 19:36:45 +0100

    Improve codegen slightly (#34)
    * improve codegen - don't regenerate files unless template has changed
    
    * fix improper usage of stat in codegen

--- utils/codegen/codegen.js
+++ utils/codegen/codegen.js
@@ -11,12 +11,20 @@ env.addFilter('camelCase', str => str.charAt(0).toLowerCase() + str.substr(1));
 for (const source of [
   'AST/ASTPass/ASTPass'
 ]) {
-  console.log(`Processing ${source}.swift`)
+  let sourcePath = `Sources/${source}.template.swift`;
+  let resultPath = `.derived-sources/${source}.swift`;
+  let sourceTime = fs.statSync(sourcePath).mtimeMs;
+  let resultTime = fs.existsSync(resultPath) ? fs.statSync(resultPath).mtimeMs : 0;
+  if (sourceTime <= resultTime) {
+    console.log(`Skipping ${source}.swift`);
+    continue;
+  }
+  console.log(`Processing ${source}.swift ...`);
   mkdirp.sync(`.derived-sources/${path.dirname(source)}`);
   fs.writeFileSync(
-    `.derived-sources/${source}.swift`,
+    resultPath,
     env.renderString(
-      fs.readFileSync(`Sources/${source}.template.swift`, 'utf8')
+      fs.readFileSync(sourcePath, 'utf8')
     )
   );
 }
