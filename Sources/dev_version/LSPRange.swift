bb95e7e8f3bed6ca95df46a27bbb86f656e55d62
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-01-16 18:04:44 +0000

    Filled out defenition for LSPRange

--- Sources/dev_version/LSPRange.swift
+++ Sources/dev_version/LSPRange.swift
@@ -1,3 +1,20 @@
 public struct LSPRange {
+    private struct Position
+    {
+        private var LineNum : Int
+        private var ColumnNum : Int
+        
+        init(lineNum: Int, columnNum: Int) {
+            LineNum = lineNum
+            ColumnNum = columnNum
+        }
+    }
     
+    private var Start : Position
+    private var End : Position
+    
+    init(startLineNum: Int, startColumNum: Int, endLineNum: Int, endColumnNum: Int) {
+        Start = Position(lineNum: startLineNum, columnNum: startColumNum)
+        End = Position(lineNum: endLineNum, columnNum: endColumnNum)
+    }
 }
