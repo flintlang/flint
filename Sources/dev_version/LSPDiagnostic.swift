3c886d362193657c2cffd467329fb98b7d52284c
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-01-16 17:53:09 +0000

    Add definitions for LSP diagnostic structures

--- Sources/dev_version/LSPDiagnostic.swift
+++ Sources/dev_version/LSPDiagnostic.swift
@@ -1,4 +1,7 @@
 public struct LSPDiagnostic {
+    
+    private var range : LSPRange
+    
     private enum Severity: Int
     {
         case Error = 1
@@ -7,7 +10,12 @@ public struct LSPDiagnostic {
         case Hint = 4
     }
     
+    private var code : String?
     
+    private var source : String?
     
+    private var message : String
+    
+    private var RelatedInformation : [LSPDiagnosticRelatedInformation]
     
 }
---
+++ Sources/dev_version/LSPDiagnosticRelatedInformation.swift
@@ -0,0 +1,5 @@
+public struct LSPDiagnosticRelatedInformation {
+    private var location : LSPRange
+    
+    private var message : String    
+}
