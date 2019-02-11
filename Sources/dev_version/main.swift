5f53092f72138928090ea0a9e161460ec895c9a0
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-02-11 18:22:35 +0000

    Update main to use non debug version of main function so it works LS

--- Sources/dev_version/main.swift
+++ Sources/dev_version/main.swift
@@ -6,7 +6,6 @@ import Diagnostic
 
 /// The main function for the compiler.
 func main() {
-    
     command (
         Argument<String> ("source code", description: "source code to verify"),
         Argument<String> ("file name", description: "file name")
@@ -38,6 +37,36 @@ func main() {
             exit(1)
         }
     }.run() 
+}
+
+func main_d() throws {
+        let fileName = "/Users/Zubair/Documents/Imperial/Thesis/Code/flint/ide_examples/syntactically_incorrect_let_declaration.flint"
+        let inputFiles = [URL(fileURLWithPath: fileName)]
+        let sourceCode = try String(contentsOf: inputFiles[0])
+        do {
+            //print("this  is the first time running")
+            let c = Compiler(
+                sourceFiles: inputFiles,
+                sourceCode: sourceCode,
+                stdlibFiles: StandardLibrary.default.files,
+                diagnostics: DiagnosticPool(shouldVerify: false,
+                                            quiet: false,
+                                            sourceContext: SourceContext(sourceFiles:inputFiles, sourceCodeString: sourceCode, isForServer: true)))
+            
+            try c.ide_compile()
+            let diag = c.diagnostics
+            let json = try convertFlintDiagToLspDiagJson(diag.getDiagnostics())
+            print(json)
+            
+        } catch let err {
+            let diagnostic = Diagnostic(severity: .error,
+                                        sourceLocation: nil,
+                                        message: err.localizedDescription)
+            // swiftlint:disable force_try
+            print(try! DiagnosticsFormatter(diagnostics: [diagnostic], sourceContext: nil).rendered())
+            // swiftlint:enable force_try
+            exit(1)
+        }
 }
 
 main()
