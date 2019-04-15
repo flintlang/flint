5ebfa5801f81e58c8b5e78941ef5c5fcfed71c29
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-15 16:08:16 +0100

    JSTestSuite.swift: Create functions to process contract + contract behaviours

--- Sources/JSTranslator/JSTestSuite.swift
+++ Sources/JSTranslator/JSTestSuite.swift
@@ -1,7 +1,6 @@
 import AST
 
 public class JSTestSuite {
-    
     private var contractName: String
     private var filePath: String
     private var JSTestFuncs: [JSTestFunction]
@@ -20,15 +19,23 @@ public class JSTestSuite {
         for d in declarations {
             switch d {
             case .contractDeclaration(let contractDec):
-                print(contractDec)
+                processContract(contract: contractDec)
             case .contractBehaviorDeclaration(let contractBehaviour):
-                print(contractBehaviour)
+                processContractBehaviour(contractBehaviour: contractBehaviour)
             default:
                 continue
             }
         }
-        
-        
+    }
+    
+    private func processContractBehaviour(contractBehaviour: ContractBehaviorDeclaration)
+    {
+        // this function should extract out the test funcs from each of the contracts and populate the JSTestFuncs array
+    }
+    
+    private func processContract(contract : ContractDeclaration)
+    {
+        // this functions duty is to extract the file path and contract name
     }
     
     // this is the function that generates the string representation of the JS file -> ready for execution
