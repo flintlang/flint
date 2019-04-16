4bafd11ae2226b21a3bf5abc14b646c60ab5ff9a
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 20:19:45 +0100

    JSNodes.swift: Add description for JSNodes

--- Sources/JSTranslator/JSNodes.swift
+++ Sources/JSTranslator/JSNodes.swift
@@ -1,6 +1,19 @@
-public enum JSNode {
+public enum JSNode : CustomStringConvertible {
     case FunctionCall(JSFunctionCall)
     case VariableAssignment(JSVariableAssignment)
     case Variable(JSVariable)
     case Literal(JSLiteral)
+    
+    public var description: String {
+        switch (self) {
+        case .Literal(let li):
+            return li.description
+        case .VariableAssignment(let vAssignment):
+            return vAssignment.description
+        case .Variable(let vari):
+            return vari.description
+        case .FunctionCall(let fCall):
+            return fCall.description
+        }
+    }
 }
