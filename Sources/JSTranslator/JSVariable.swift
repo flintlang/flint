e5d1aff444b8964cad2fdf6f9fb9e5fcc0b4989d
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 20:18:33 +0100

    JSVariable.swift: Add description for variable

--- Sources/JSTranslator/JSVariable.swift
+++ Sources/JSTranslator/JSVariable.swift
@@ -1,7 +1,11 @@
-public class JSVariable {
+public class JSVariable : CustomStringConvertible {
     private let variable : String
     
     public init(variable: String) {
         self.variable = variable
+    }
+    
+    public var description: String {
+        return variable
     }
 }
