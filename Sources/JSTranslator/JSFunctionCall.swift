4750d120f0dccc77dc91d60aac138b56418753aa
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 16:05:17 +0100

    JSFunctionCall.swift: Add contract name + function arguments as class members

--- Sources/JSTranslator/JSFunctionCall.swift
+++ Sources/JSTranslator/JSFunctionCall.swift
@@ -3,11 +3,15 @@ public class JSFunctionCall {
     private let transactionMethod : Bool
     private let isAssert: Bool
     private let functionName: String
+    private let args : [JSNode]
+    private let contractName : String
     
-    public init(contractCall: Bool, transactionMethod: Bool, isAssert: Bool, functionName: String) {
+    public init(contractCall: Bool, transactionMethod: Bool, isAssert: Bool, functionName: String, contractName : String, args : [JSNode]) {
         self.contractCall = contractCall
         self.transactionMethod = transactionMethod
         self.isAssert = isAssert
         self.functionName = functionName
+        self.contractName = contractName
+        self.args = args
     }
 }
