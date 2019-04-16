657cf54f02b26b24788e35ff3b634467940b6879
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 20:50:50 +0100

    JSVariableAssignment.swift: Add comment syntax in front of instantiations

--- Sources/JSTranslator/JSVariableAssignment.swift
+++ Sources/JSTranslator/JSVariableAssignment.swift
@@ -2,15 +2,27 @@ public class JSVariableAssignment : CustomStringConvertible {
     private let lhs : String
     private let isConstant: Bool
     private let rhs : JSNode
+    private let isInstantiation: Bool
     
-    public init(lhs: String, rhs: JSNode, isConstant: Bool) {
+    public init(lhs: String, rhs: JSNode, isConstant: Bool, isInstantiation : Bool = false) {
         self.lhs = lhs
         self.rhs = rhs
         self.isConstant = isConstant
+        self.isInstantiation = isInstantiation
     }
     
     public var description: String {
+        
+        var desc : String = ""
+        
+        if (isInstantiation)
+        {
+            desc += "//"
+        }
+
         let varModifier = isConstant ? "let" : "var"
-        return varModifier + " " + lhs.description + " = " + rhs.description
+        desc += varModifier
+        desc += " " + lhs.description + " = " + rhs.description
+        return desc
     }
 }
