1857ec7632f1ad697fcb6c6da5ae60bcf0b7693e
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 20:50:22 +0100

    JSFunctionCall.swift: Add description to JSFunctionCall

--- Sources/JSTranslator/JSFunctionCall.swift
+++ Sources/JSTranslator/JSFunctionCall.swift
@@ -1,10 +1,13 @@
-public class JSFunctionCall {
+public class JSFunctionCall : CustomStringConvertible {
+    
     private let contractCall : Bool
     private let transactionMethod : Bool
     private let isAssert: Bool
     private let functionName: String
     private let args : [JSNode]
     private let contractName : String
+    
+    // return type of function call is required
     
     public init(contractCall: Bool, transactionMethod: Bool, isAssert: Bool, functionName: String, contractName : String, args : [JSNode]) {
         self.contractCall = contractCall
@@ -13,5 +16,55 @@ public class JSFunctionCall {
         self.functionName = functionName
         self.contractName = contractName
         self.args = args
+    }
+    
+    private func create_arg_list() -> String {
+        var argList = ""
+        let lastIndex = args.count - 1
+        var counter = 0
+        for a in args {
+            if counter == lastIndex {
+                argList += a.description
+                continue
+            }
+            argList +=  a.description + ", "
+            counter += 1
+        }
+    
+        return argList
+    }
+    
+    public var description: String {
+        // this is where you actually call the right
+        var fCall = ""
+        
+        if (contractCall) {
+            if (transactionMethod) {
+                fCall = "await transactional_method(t_contract, " + "'" + self.functionName + "'"
+                if args.count > 0 {
+                    fCall += "," + "[" + create_arg_list() + "]" + ")"
+                } else {
+                    fCall += ", [])"
+                }
+            } else {
+                fCall = "call_method_int(t_contract, " + self.functionName
+                if args.count > 0 {
+                    fCall += "," + create_arg_list() + ")"
+                } else {
+                    fCall += ", [])"
+                }
+            }
+        } else {
+            fCall = self.functionName + "("
+            
+            if args.count > 0 {
+                fCall += create_arg_list() + ")"
+            } else {
+                fCall += ")"
+            }
+            
+        }
+    
+        return fCall
     }
 }
