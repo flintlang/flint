9958bf4c4efecabc7733543b4ecb3c13a2267907
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 20:17:54 +0100

    JSLiteral.swift: Add description function for literals

--- Sources/JSTranslator/JSLiteral.swift
+++ Sources/JSTranslator/JSLiteral.swift
@@ -1,3 +1,10 @@
-public enum JSLiteral {
+public enum JSLiteral : CustomStringConvertible {
    case Integer(Int)
+    
+    public var description: String {
+        switch (self) {
+        case .Integer(let i):
+            return i.description
+        }
+    }
 }
