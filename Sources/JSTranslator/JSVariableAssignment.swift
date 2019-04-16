10ff015181f189a1d4d1b710fc1ab1994fea2771
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-16 16:05:52 +0100

    JSVariableAssignment.swift: Add type of variable (let or var)  assignment to class

--- Sources/JSTranslator/JSVariableAssignment.swift
+++ Sources/JSTranslator/JSVariableAssignment.swift
@@ -1,10 +1,12 @@
 public class JSVariableAssignment {
     private let lhs : String
+    private let isConstant: Bool
     private let rhs : JSNode
     
-    public init(lhs: String, rhs: JSNode) {
+    public init(lhs: String, rhs: JSNode, isConstant: Bool) {
         self.lhs = lhs
         self.rhs = rhs
+        self.isConstant = isConstant
     }
     
 }
