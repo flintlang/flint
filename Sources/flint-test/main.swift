fc60f5b7cee96b8342b21151a56ba5c0df655f06
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-17 17:57:16 +0100

    flint-test/main.swift: Modify main_d function to run the test runner

--- Sources/flint-test/main.swift
+++ Sources/flint-test/main.swift
@@ -2,6 +2,7 @@ import Foundation
 import Commander
 import AST
 import LSP
+import JSTranslator
 import Diagnostic
 
 /// The main function for the compiler.
@@ -39,31 +40,20 @@ func main() {
 }
 
 func main_d() throws {
-        let fileName = "/Users/Zubair/Documents/Imperial/Thesis/Code/flint_testing_framework_older_webjs/examples/test_counter.tflint"
-        let inputFiles = [URL(fileURLWithPath: fileName)]
-        let sourceCode = try String(contentsOf: inputFiles[0])
-        do {
-            //print("this  is the first time running")
-            let c = Compiler(
-                sourceFiles: inputFiles,
-                sourceCode: sourceCode,
-                stdlibFiles: StandardLibrary.default.files,
-                diagnostics: DiagnosticPool(shouldVerify: false,
-                                            quiet: false,
-                                            sourceContext: SourceContext(sourceFiles:inputFiles, sourceCodeString: sourceCode, isForServer: true)),
-                typeStateDiagram : false)
-            
-            try c.ide_compile()
-            
-        } catch let err {
-            let diagnostic = Diagnostic(severity: .error,
-                                        sourceLocation: nil,                        
-                                        message: err.localizedDescription)
-            // swiftlint:disable force_try
-            print(try! DiagnosticsFormatter(diagnostics: [diagnostic], sourceContext: nil).rendered())
-            // swiftlint:enable force_try
-            exit(1)
-        }
+    let fileName = "/Users/Zubair/Documents/Imperial/Thesis/Code/flint_testing_framework_older_webjs/examples/test_counter.tflint"
+    let inputFiles = [URL(fileURLWithPath: fileName)]
+    let sourceCode = try String(contentsOf: inputFiles[0])
+    
+    do {
+        try TestRunner(testFile: inputFiles[0],
+                       sourceCode: sourceCode,
+                       diagnostics: DiagnosticPool(shouldVerify: false,
+                                                   quiet: false,
+                                                   sourceContext: SourceContext(sourceFiles: inputFiles))).run_tests()
+        
+    } catch {
+        print("Failed to run tests")
+    }
 }
 
 //try main_d()
