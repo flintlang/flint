881510d1e892cc73044bd01a148a0819e22c833e
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-17 14:37:40 +0100

    JSTestSuite.swift: Generate entire javascript file which can now run tests

--- Sources/JSTranslator/JSTestSuite.swift
+++ Sources/JSTranslator/JSTestSuite.swift
@@ -12,6 +12,104 @@ public class JSTestSuite {
     
     private var isFuncTransaction : [String:Bool]
     private var contractFunctionNames : [String]
+    
+    private let firstHalf : String =
+"""
+const Web3 = require('web3');
+const fs = require('fs');
+const path = require('path');
+const solc = require('solc');
+const chalk = require('chalk');
+const web3 = new Web3();
+const eth = web3.eth;
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+var accountAdd = web3.personal.newAccount("1");
+web3.personal.unlockAccount(accountAdd, "1", 1000);
+web3.eth.defaultAccount = accountAdd;
+
+async function deploy_contract(abi, bytecode) {
+    let gasEstimate = eth.estimateGas({data: bytecode});
+    let localContract = eth.contract(JSON.parse(abi));
+
+    return new Promise (function(resolve, reject) {
+    localContract.new({
+      from:accountAdd,
+      data:bytecode,
+      gas:gasEstimate}, function(err, contract){
+       if(!err) {
+          // NOTE: The callback will fire twice!
+          // Once the contract has the transactionHash property set and once its deployed on an address.
+           // e.g. check tx hash on the first call (transaction send)
+          if(!contract.address) {
+          //console.log(contract.transactionHash) // The hash of the transaction, which deploys the contract
+         
+          // check address on the second call (contract deployed)
+          } else {
+              //newContract = myContract;
+              //contractDeployed = true;
+              // setting the global instance to this contract
+              resolve(contract);
+          }
+           // Note that the returned "myContractReturned" === "myContract",
+          // so the returned "myContractReturned" object will also get the address set.
+       }
+     });
+    });
+}
+
+async function check_tx_mined(tx_hash) {
+    let txs = eth.getBlock("latest").transactions;
+    return new Promise(function(resolve, reject) {
+        while (!txs.includes(tx_hash)) {
+            txs = eth.getBlock("latest").transactions;
+        }
+        resolve("true");
+    });
+}
+
+async function transactional_method(contract, methodName, args) {
+    var tx_hash = await new Promise(function(resolve, reject) {
+        contract[methodName]['sendTransaction'](...args, function(err, result) {
+            resolve(result);
+        });
+    });
+
+    let isMined = await check_tx_mined(tx_hash);
+
+    return new Promise(function(resolve, reject) {
+        resolve(isMined);
+    });
+}
+
+function call_method_int(contract, methodName, args) {
+    return contract[methodName]['call'](...args).toNumber();
+}
+
+function assertEqual(result_dict, expected, actual) {
+    let result = expected === actual;
+    result_dict['result'] = result && result_dict['result'];
+
+    // do I want to keep updating the msg
+    if (result) {
+        result_dict['msg'] = "has Passed";
+    } else {
+        result_dict['msg'] = "has Failed";
+    }
+
+    return result_dict
+}
+
+function process_test_result(res, test_name) {
+    if (res['result'])
+    {
+        console.log(chalk.green(test_name + " " + res['msg']));
+    } else {
+        console.log(chalk.red(test_name + " " +  res['msg']));
+    }
+}
+
+"""
 
     // creates the JSTestSuite class
     public init() {
@@ -310,10 +408,21 @@ public class JSTestSuite {
     }
     
     private func genRunTests() -> String {
-        var fnc = "async function run_tests() {\n"
+        var fnc = "async function run_tests(pathToContract, nameOfContract) {\n"
         
+        fnc += "    let source = fs.readFileSync(pathToContract, 'utf8'); \n"
+        
+        fnc += "    let compiledContract = solc.compile(source, 1); \n"
+        
+        fnc += "    let abi = compiledContract.contracts[':_Interface' + nameOfContract].interface; \n"
+        
+        fnc += "    let bytecode = \"0x\" + compiledContract.contracts[':' + nameOfContract].bytecode; \n"
+        
+        var counter: Int = 0
         for tFnc in JSTestFuncs {
-            fnc += "    await "  + tFnc.getFuncName() + "(myContract) \n"
+            fnc += "    let depContract_\(counter) = await deploy_contract(abi, bytecode); \n"
+            fnc += "    await "  + tFnc.getFuncName() + "(depContract_\(counter)) \n"
+            counter += 1
         }
         
         fnc += "}\n\n"
@@ -322,7 +431,7 @@ public class JSTestSuite {
     
     // this is the function that generates the string representation of the JS file -> ready for execution
     public func genFile() -> String {
-        var file = ""
+        var file = firstHalf
         for testFunc in JSTestFuncs {
             file += testFunc.description + "\n"
         }
@@ -331,11 +440,14 @@ public class JSTestSuite {
         
         file += genRunTests()
         
-        file += "function delay_tests() { \n        filter_latest.stopWatching(); \n        setTimeout(run_tests, 1000); \n} \n\n"
-        
-        file += "const filter_latest = eth.filter(\"latest\", delay_tests); \n"
-        
-        file += "deploy_contract('main.sol', '\(self.contractName)');"
+        file +=
+        """
+        function  main(pathToContract, nameOfContract) {
+            run_tests(pathToContract, nameOfContract)
+        } \n\n
+        """
+    
+        file += "main('main.sol', '\(self.contractName)');"
         
         print(file)
     
