eb40f0523aa5c1c88d7a1d1e03bde05b1e39d50d
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-23 12:42:18 +0100

    JSLiteral.swift: Add support for string literals

--- Sources/JSTranslator/JSLiteral.swift
+++ Sources/JSTranslator/JSLiteral.swift
@@ -1,10 +1,13 @@
 public enum JSLiteral : CustomStringConvertible {
    case Integer(Int)
+   case String(String)
     
     public var description: String {
         switch (self) {
         case .Integer(let i):
             return i.description
+        case .String(let s):
+            return "\"" + s + "\""
         }
     }
 }
