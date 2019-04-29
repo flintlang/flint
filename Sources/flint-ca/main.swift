e8a4128a978789a40cefb11397adf81bb1d036c2
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-04-29 19:17:56 +0100

    flint-ca/main.swift: Call analyser module

--- Sources/flint-ca/main.swift
+++ Sources/flint-ca/main.swift
@@ -14,30 +14,17 @@ func main() {
         Argument<String> ("file name", description: "file name")
     )
     { typeStateDiagram, callerAnalysis, estimateGas, sourceCode, fileName in
-        let inputFiles = [URL(fileURLWithPath: fileName)]
+        
         do {
-            let c = Compiler(
-                sourceFiles: inputFiles,
-                sourceCode: sourceCode,
-                stdlibFiles: StandardLibrary.default.files,
-                diagnostics: DiagnosticPool(shouldVerify: false,
-                                            quiet: false,
-                        sourceContext: SourceContext(sourceFiles:inputFiles, sourceCodeString: sourceCode, isForServer: true)),
-                typeStateDiagram: typeStateDiagram,
-                callerCapabilityAnalysis: callerAnalysis,
-                estimateGas: estimateGas)
-            
-            // I need a better way of representing the flags
-            try c.ide_compile()
+            let a = Analyser(contractFile: fileName,
+                             sourceCode: sourceCode,
+                             estimateGas: estimateGas,
+                             typeStateDiagram: typeStateDiagram,
+                             callerCapabilityAnalysis: callerAnalysis)
+            try a.analyse()
             
         } catch let err {
-            let diagnostic = Diagnostic(severity: .error,
-                                        sourceLocation: nil,
-                                        message: err.localizedDescription)
-            // swiftlint:disable force_try
-            print(try! DiagnosticsFormatter(diagnostics: [diagnostic], sourceContext: nil).rendered())
-            // swiftlint:enable force_try
-            exit(1)
+            print(err)
         }
     }.run() 
 }
@@ -46,29 +33,17 @@ func main_d() throws {
         let fileName = "/Users/Zubair/Documents/Imperial/Thesis/Code/flint/ide_examples/curr_examples/test.flint"
         let inputFiles = [URL(fileURLWithPath: fileName)]
         let sourceCode = try String(contentsOf: inputFiles[0])
+    
         do {
-            //print("this  is the first time running")
-            let c = Compiler(
-                sourceFiles: inputFiles,
-                sourceCode: sourceCode,
-                stdlibFiles: StandardLibrary.default.files,
-                diagnostics: DiagnosticPool(shouldVerify: false,
-                                            quiet: false,
-                                            sourceContext: SourceContext(sourceFiles:inputFiles, sourceCodeString: sourceCode, isForServer: true)),
-                typeStateDiagram : false,
-                callerCapabilityAnalysis: false,
-                estimateGas: true)
-            
-            try c.ide_compile()
-            
+             let a = Analyser(contractFile: fileName,
+                         sourceCode: sourceCode,
+                         estimateGas: true,
+                         typeStateDiagram: false,
+                         callerCapabilityAnalysis: false)
+             try a.analyse()
+        
         } catch let err {
-            let diagnostic = Diagnostic(severity: .error,
-                                        sourceLocation: nil,                        
-                                        message: err.localizedDescription)
-            // swiftlint:disable force_try
-            print(try! DiagnosticsFormatter(diagnostics: [diagnostic], sourceContext: nil).rendered())
-            // swiftlint:enable force_try
-            exit(1)
+          print(err)
         }
 }
 
