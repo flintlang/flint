7f0190b3543fa53ee2cc7605ee8b401416dcee14
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-08 16:07:17 +0100

    REPL.swift: Add empty read eval loop:

--- Sources/REPL/REPL.swift
+++ Sources/REPL/REPL.swift
@@ -2,12 +2,12 @@ public class REPL {
     let contractInfoMap : [String : ContractInfo] = [:]
     
     public init(contractFilePath: String, contractAddress : String = "") {
-       print(contractFilePath)
-       print(contractAddress)
     }
     
     public func run() throws {
-        print("repl is running")
+        while let input = readLine() {
+            print(input)
+        }
     }
     
 }
