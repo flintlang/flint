9516bba850b1a331bb0b954c339d002315eceacd
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-11 14:49:36 +0100

    REPLContract.swift: Remove JSTranslator functions from REPLContract

--- Sources/REPL/REPLContract.swift
+++ Sources/REPL/REPLContract.swift
@@ -162,7 +162,7 @@ public class REPLContract{
         
         let data = pipe.fileHandleForReading.readDataToEndOfFile();
         if let addr = String(data: data, encoding: .utf8) {
-            instanceToAddress[variable_name] = addr
+            //instanceToAddress[variable_name] = addr
         } else {
             print("ERROR : Could not deploy contract \(self.contractName)")
         }
@@ -200,217 +200,14 @@ public class REPLContract{
                     if let resultType = allFuncsWithName[0].declaration.signature.resultType {
                         contractFunctionInfo[fName] = ContractFuncInfo(resultType: resultType.name)
                     }
-                    
+                
                     contractFunctionNames.append(fName)
                 }
             }
-            
-            
         } catch {
             print("Fatal error")
             exit(1)
         }
     }
-    
-    private func extract_literal(literalToken : Token) -> JSNode? {
-        switch (literalToken.kind) {
-        case .literal(let lit):
-            switch (lit) {
-            case .decimal(let dec):
-                switch (dec) {
-                case .integer(let val):
-                    return .Literal(.Integer(val))
-                default:
-                    break
-                }
-            case .address(let s):
-                return .Literal(.String(s))
-            case .string(let s):
-                return .Literal(.String(s))
-            default:
-                return nil
-            }
-        default:
-            return nil
-        }
-        
-        return nil
-    }
-    
-    
-    private func process_func_call_args(args : [FunctionArgument]) -> [JSNode] {
-        
-        var jsArgs : [JSNode] = []
-        
-        for a in args {
-            // create a JSNode for each of these but for now we will just do variables
-            switch (a.expression)
-            {
-            case .identifier(let i):
-                jsArgs.append(.Variable(JSVariable(variable: i.name)))
-            case .literal(let l):
-                if let lit = extract_literal(literalToken: l) {
-                    jsArgs.append(lit)
-                }
-            case .binaryExpression(let be):
-                switch (be.opToken) {
-                case .dot:
-                    jsArgs.append(process_dot_expr(binExpr: be))
-                default:
-                    break
-                }
-                
-            default:
-                break
-            }
-        }
-        
-        return jsArgs
-    }
-    
-    private func process_assignment_expr(binExp : BinaryExpression) -> JSNode
-    {
-        var rhsNode : JSNode? = nil
-        var type : Bool? = nil
-        var name: String? = nil
-        var resultType: String? = nil
-        var isInstantiation : Bool = false
-        
-        
-        switch (binExp.lhs) {
-        case .variableDeclaration(let vdec):
-            name = vdec.identifier.name
-            type = vdec.isConstant
-            switch (vdec.type.rawType) {
-            case .basicType(let rt):
-                switch (rt) {
-                case .string:
-                    resultType = "String"
-                case .int:
-                    resultType = "Int"
-                case .address:
-                    resultType = "Address"
-                case .bool:
-                    resultType = "Bool"
-                default:
-                    resultType = vdec.type.rawType.name
-                }
-            default:
-                resultType = vdec.type.rawType.name
-            }
-        default:
-            break
-        }
-        
-        
-        switch (binExp.rhs) {
-        case .binaryExpression(let binExpr):
-            switch (binExpr.op.kind) {
-            case .punctuation(let p):
-                switch (p) {
-                case .dot:
-                    rhsNode = process_dot_expr(binExpr: binExpr)
-                default:
-                    break
-                }
-            default:
-                break
-            }
-        case .functionCall(let fCall):
-            isInstantiation = !fCall.identifier.name.lowercased().contains("assert") && !contractFunctionNames.contains(fCall.identifier.name) && fCall.identifier.name.lowercased().contains(self.contractName.lowercased())
-            rhsNode = process_func_call(fCall: fCall)
-        case .literal(let li):
-            if let lit = extract_literal(literalToken: li) {
-                rhsNode = lit
-            }
-        default:
-            break
-        }
-        
-        return .VariableAssignment(JSVariableAssignment(lhs: name!, rhs: rhsNode!, isConstant: type!, resultType: resultType!, isInstantiation: isInstantiation))
-    }
-    
-    private func process_dot_expr(binExpr : BinaryExpression) -> JSNode {
-        var lhsName : String = ""
-        var rhsNode : JSNode? = nil
-        
-        switch (binExpr.lhs) {
-        case .identifier(let i):
-            lhsName = i.name
-        default:
-            break
-        }
-        
-        switch (binExpr.rhs) {
-        case .functionCall(let fCall):
-            rhsNode = process_func_call(fCall: fCall, lhsName: lhsName)
-        case .identifier(let i):
-            rhsNode = .Variable(JSVariable(variable: i.name))
-        default:
-            break
-        }
-        
-        return rhsNode!
-    }
-    
-    private func process_func_call(fCall : FunctionCall, lhsName: String = "") -> JSNode {
-        let fName : String = fCall.identifier.name
-        let funcArgs = process_func_call_args(args: fCall.arguments)
-        var isTransaction = false
-        
-        if let isFuncTransaction = isFuncTransaction[fName] {
-            isTransaction = isFuncTransaction
-        }
-        
-        var resultType: String = ""
-        if let funcInfo = contractFunctionInfo[fName] {
-            resultType = funcInfo.getType()
-        }
-        
-        var contractEventInfo : ContractEventInfo? = nil
-        if fName.contains("assertEventFired") {
-            if let eventInfo = self.contractEventInfo[funcArgs[0].description] {
-                contractEventInfo = eventInfo
-            } else {
-                print("The event " + funcArgs[0].description + " does not exist")
-                exit(0)
-            }
-        }
-        
-        let isAssert = fName.lowercased().contains("assert")
-        
-        return .FunctionCall(JSFunctionCall(contractCall: contractFunctionNames.contains(fName), transactionMethod: isTransaction, isAssert: isAssert, functionName: fName, contractName: lhsName, args: funcArgs, resultType: resultType, eventInformation: contractEventInfo))
-    }
-    
-    
-    public func process_expr(expr : Expression) -> JSNode
-    {
-        var jsNode : JSNode? = nil
-        switch (expr) {
-        case .binaryExpression(let binExp):
-            switch (binExp.op.kind) {
-            case .punctuation(let punc):
-                switch (punc) {
-                case .equal:
-                    jsNode = process_assignment_expr(binExp: binExp)
-                case .dot:
-                    jsNode = process_dot_expr(binExpr: binExp)
-                default:
-                    break
-                }
-            default: break
-            }
-        case .functionCall(let fCall):
-            jsNode = process_func_call(fCall: fCall)
-        default:
-            break
-        }
-        
-        return jsNode!
-    }
-
-    
-    
-    
     
 }
