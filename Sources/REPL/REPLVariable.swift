40eb246866920f6b33b6e7fbd0375dcf994032f8
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-12 18:03:34 +0100

    REPLVariable.swift: Add constant flag to variable

--- Sources/REPL/REPLVariable.swift
+++ Sources/REPL/REPLVariable.swift
@@ -2,10 +2,12 @@ public struct REPLVariable {
     public let variableName : String
     public let variableType : String
     public let variableValue : String
+    public let varConstant : Bool
     
-    public init(variableName : String, variableType : String, variableValue : String) {
+    public init(variableName : String, variableType : String, variableValue : String, varConstant : Bool) {
         self.variableName = variableName
         self.variableType = variableType
         self.variableValue = variableValue
+        self.varConstant = varConstant 
     }
 }
