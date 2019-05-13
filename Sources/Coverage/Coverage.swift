00276481e537849c58b09a77e7ce4d40b8701621
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-14 00:39:45 +0100

    Coverage.swift: Add skeleton function called instrument

--- Sources/Coverage/Coverage.swift
+++ Sources/Coverage/Coverage.swift
@@ -1,3 +1,15 @@
+import AST
+
 public class CoverageProvider {
+    private var executableStatementCount : Int = 0
+    private var functionsCount : Int = 0
+    private var branchNumberCount : Int = 0
+    
+    public init() {}
+    
+    public func instrument(ast : TopLevelModule) -> TopLevelModule {
+     
+        return ast
+    }
 
 }
