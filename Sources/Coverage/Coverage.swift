d2484c4e907d4ddd1edc7abf08e2be92f6df44c8
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-14 01:11:52 +0100

    Coverage.swift: Add main loop in the instrument function, add skeletons for helper functions

--- Sources/Coverage/Coverage.swift
+++ Sources/Coverage/Coverage.swift
@@ -8,8 +8,28 @@ public class CoverageProvider {
     public init() {}
     
     public func instrument(ast : TopLevelModule) -> TopLevelModule {
+        var new_decs : [TopLevelDeclaration] = []
+        
+        for dec in ast.declarations {
+            switch (dec) {
+            case .contractDeclaration(let cdec):
+                new_decs.append(.contractDeclaration(instrument_contract_dec(cdec: cdec)))
+            case .contractBehaviorDeclaration(let cbdec):
+                new_decs.append(.contractBehaviorDeclaration(instrument_contract_b_dec(cBdec: cbdec)))
+            default:
+                new_decs.append(dec)
+            }
+        }
      
-        return ast
+        return TopLevelModule(declarations: new_decs)
+    }
+    
+    private func instrument_contract_dec(cdec: ContractDeclaration) -> ContractDeclaration {
+        return cdec
+    }
+    
+    private func instrument_contract_b_dec(cBdec: ContractBehaviorDeclaration) -> ContractBehaviorDeclaration {
+        return cBdec
     }
 
 }
