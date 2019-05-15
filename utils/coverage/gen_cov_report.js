d8ee29358c42663ae09b2188b28b30a516004212
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-15 17:48:27 +0100

    utils/coverage/gen_cov_report.js: Update script to gather all instrumentation events and produce a coverage.info report

--- utils/coverage/gen_cov_report.js
+++ utils/coverage/gen_cov_report.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); 
 const solc = require('solc');
 const web3 = new Web3();
 const eth = web3.eth;
@@ -23,55 +23,113 @@ async function getEvents(contract, eventName, event_args) {
 	});
 }
 
-function process_stmc_event_logs(logs) {
-	res = []
+function process_stmc_event_logs(logs, result_dict, found_counts) {
+
 	logs.forEach(function(entry) {
 		let lineNo = entry.args["line"].toNumber();
-		res.push("DA:" + lineNo + ",1");
+		if (lineNo in result_dict["DA"]) {
+			result_dict["DA"][lineNo].count += 1
+		} else {
+			result_dict["DA"][lineNo] = {lineNo: lineNo, count: 1}
+			found_counts.DA += 1
+		}
 	});
-
-	return res
 }
 
-function process_funcC_event_logs(logs) {
-	res = []
+function process_funcC_event_logs(logs, result_dict, found_counts) {
 	logs.forEach(function(entry) {
 		let fncName = getString(entry.args["fName"]);
-		res.push("FNDA:1," + fncName)
+		if (fncName in result_dict["functions"]) {
+			result_dict["functions"][fncName].count += 1
+		} else {
+			result_dict["functions"][fncName] = {fncName: fncName, count: 1};
+			found_counts.functions += 1
+		}
 	});
-
-	return res
 }
 
-function process_branchC_event_logs(logs) {
-	res = []
+function process_branchC_event_logs(logs, result_dict, found_counts) {
 	logs.forEach(function(entry) {
 		let lineNo = entry.args["line"].toNumber();
 		let branchNum = entry.args["branch"].toNumber();
 		let blockNum = entry.args["blockNum"].toNumber();
-		res.push("BRDA:" + lineNo + "," + blockNum + "," + branchNum + "1")
+		if (lineNo in result_dict["branch"]) {
+			result_dict["branch"][lineNo].count += 1
+		} else {
+			result_dict["branch"][lineNo] = {lineNo: lineNo, blockNum: blockNum, branchNum: branchNum, count: 1}
+			found_counts.branch += 1
+		}
 	});
-
-	return res
 }
 
-async function main() { 
-	let abi = JSON.parse(fs.readFileSync("contract.json").toString())["contracts"][":_InterfaceCounter1"].interface
-	let address = "0x986abb64f2d331a63ad47f16a3d59fe8184f0467"
+async function get_events(abi, address, result_dict, found_counts) {
 	let localContract = eth.contract(JSON.parse(abi));
 	let instance = localContract.at(address);
 	let stmt = await getEvents(instance, "stmC", {});
 	let fncC = await getEvents(instance, "funcC", {});
 	let branchC = await getEvents(instance, "branchC", {});
-	let sLogs = process_stmc_event_logs(stmt);	
-	let fLogs = process_funcC_event_logs(fncC);	
-	let bLogs = process_branchC_event_logs(branchC);	
+	let sLogs = process_stmc_event_logs(stmt, result_dict, found_counts);	
+	let fLogs = process_funcC_event_logs(fncC, result_dict, found_counts);	
+	let bLogs = process_branchC_event_logs(branchC, result_dict, found_counts);	
+}
 
-	let res = sLogs.concat(fLogs).concat(bLogs);
-	fs.writeFileSync("coverage.info", "SF: /Users/Zubair/Documents/Imperial/Thesis/Code/flint/test_1.flint \n");
-	res.forEach(function(entry) {
+async function main() { 
+	let contractName = process.argv[2];
+	let abi = JSON.parse(fs.readFileSync("contract.json").toString())["contracts"][":_Interface" + contractName].interface
+	let addresses = fs.readFileSync("address.txt").toString().split("\n");
+
+	let res = {};
+	let found_counts = {};
+	found_counts["functions"] = 0
+	found_counts["DA"] = 0
+	found_counts["branch"] = 0
+
+	res["functions"] = {};
+	res["DA"] = {};
+	res["branch"] = {};
+
+	var i;
+	for (i = 0; i < addresses.length; i++) {
+		let addr = addresses[i].split(":")[1].trim()
+		await get_events(abi, addr, res, found_counts);
+	}
+
+	let da_records = []
+	let branch_records = []
+	let fn_records = []
+
+	Object.entries(res["functions"]).forEach(function(entry) {
+		fn_records.push("FNDA:1, " + entry[1].fncName)
+	});
+
+	Object.entries(res["branch"]).forEach(function(entry) {
+		branch_records.push("BRDA:" + entry[1].lineNo + "," + entry[1].blockNum + "," + entry[1].branchNum + "," + entry[1].count);
+	});
+
+	Object.entries(res["DA"]).forEach(function(entry) {
+		da_records.push("DA:" + entry[1].lineNo + "," + entry[1].count);
+	});
+
+	let record_set = da_records.concat(branch_records).concat(fn_records);
+
+	let counts = JSON.parse(fs.readFileSync("counts.json").toString());
+
+	let pathToContract = fs.readFileSync("contractFilePath.txt").toString();
+
+        fs.writeFileSync("coverage.info", "SF:" + pathToContract + "\n");
+
+	record_set.forEach(function(entry) {
 		fs.appendFileSync("coverage.info", entry + "\n");
 	});
+
+	fs.appendFileSync("coverage.info", "BRF:" + counts.branch  + "\n");
+	fs.appendFileSync("coverage.info", "BRH:" + found_counts.branch  + "\n");
+
+	fs.appendFileSync("coverage.info", "FNF:" + counts.functions + "\n");
+	fs.appendFileSync("coverage.info", "FNH:" + found_counts.functions + "\n");
+
+	fs.appendFileSync("coverage.info", "LF:" + counts.statements + "\n");
+	fs.appendFileSync("coverage.info", "LH:" + found_counts.DA  + "\n");
 
 	fs.appendFileSync("coverage.info", "end_of_record");
 } 
