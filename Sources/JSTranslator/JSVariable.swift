ff1a259748ac70e4875e91856d3e85459f446c2e
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-22 16:07:17 +0100

    JSVariable.swift: Add typing information to JSVariable

--- Sources/JSTranslator/JSVariable.swift
+++ Sources/JSTranslator/JSVariable.swift
@@ -1,8 +1,14 @@
 public class JSVariable : CustomStringConvertible {
     private let variable : String
+    private let type : String
     
-    public init(variable: String) {
+    public init(variable: String, type: String) {
         self.variable = variable
+        self.type = type
+    }
+    
+    public func getType() -> String {
+        return type
     }
     
     public var description: String {
