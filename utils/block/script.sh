77fd4b1282dd10c75cb54bc943e748bf7c0f943d
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-23 00:54:37 +0100

    script.sh: use local geth installation

--- utils/block/script.sh
+++ utils/block/script.sh
@@ -1,1 +1,2 @@
-geth attach http://localhost:8545 --preload ./lazyMine.js
+/Users/Zubair/Documents/Imperial/Thesis/Code/flint/utils/block/go-ethereum/build/bin/geth attach http://localhost:8545 --preload ./lazyMine.js
+
