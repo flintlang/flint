9e3f7c90d471e8ac8f94a520f4388f4a000094f3
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-22 16:04:07 +0100

    flint-repl: Add entry point for repl

--- Sources/flint-repl/main.swift
+++ Sources/flint-repl/main.swift
@@ -24,32 +24,14 @@ func main() {
 }
 
 func main_d() throws {
-    /*
-    let fileName = "/Users/Zubair/Documents/Imperial/Thesis/Code/flint/ide_examples/curr_examples/test.flint"
-    let inputFiles = [URL(fileURLWithPath: fileName)]
-    let sourceCode = try String(contentsOf: inputFiles[0])
+    
+    let repl = REPL(contractFilePath : "/Users/Zubair/Documents/Imperial/Thesis/Code/flint/wei_test.flint", contractAddress : "")
     do {
-        
-        let config = CompilerLSPConfiguration(sourceFiles: inputFiles,
-                                              sourceCode: sourceCode,
-                                              stdlibFiles: StandardLibrary.default.files,
-                                              diagnostics: DiagnosticPool(shouldVerify: false,
-                                                                          quiet: false,
-                                                                          sourceContext: SourceContext(sourceFiles: inputFiles, sourceCodeString: sourceCode, isForServer: true)))
-        let diags = try Compiler.ide_compile(config: config)
-        let lsp_json = try convertFlintDiagToLspDiagJson(diags)
-        print(lsp_json)
-        
+        try repl.run()
     } catch let err {
-        let diagnostic = Diagnostic(severity: .error,
-                                    sourceLocation: nil,
-                                    message: err.localizedDescription)
-        // swiftlint:disable force_try
-        print(try! DiagnosticsFormatter(diagnostics: [diagnostic], sourceContext: nil).rendered())
-        // swiftlint:enable force_try
-        exit(1)
+        print(err)
     }
-   */
 }
 
-main()
+//main()
+try main_d()
