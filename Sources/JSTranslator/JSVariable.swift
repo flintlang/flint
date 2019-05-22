6ba75647b810b464e976efe3384ff2b1db2d4c1c
Mohammad Zubair Chowdhury <mzc14@ic.ac.uk>
2019-05-23 00:47:29 +0100

    JSVariable.swift: Add const property to JSVariables

--- Sources/JSTranslator/JSVariable.swift
+++ Sources/JSTranslator/JSVariable.swift
@@ -1,10 +1,20 @@
 public class JSVariable : CustomStringConvertible {
     private let variable : String
     private let type : String
+    private let isConst : Bool
     
-    public init(variable: String, type: String) {
+    public init(variable: String, type: String, isConstant: Bool) {
         self.variable = variable
         self.type = type
+        self.isConst = isConstant
+    }
+    
+    public func isConstant() -> Bool {
+        return isConst
+    }
+    
+    public func name() -> String {
+        return variable
     }
     
     public func getType() -> String {
