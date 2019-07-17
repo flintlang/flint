95c1d9d41f11465665607027faeac4c1955db7a6
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2019-07-17 14:36:35 +0100

    Adding Path utility

---
+++ Sources/Utils/Path.swift
@@ -0,0 +1,20 @@
+import Foundation
+import Symbolic
+
+public class Path {
+    // Get absolute URL path from a path relative to the flint folder location
+    public static func getFullUrl(path: String) -> URL { 
+        guard var url: URL = SymbolInfo(address: #dsohandle)?.filename else {
+        fatalError("Unable to get SymbolInfo for \(#dsohandle)")
+        }
+        url.deleteLastPathComponent()
+        url.deleteLastPathComponent()
+        url.deleteLastPathComponent()
+        url.deleteLastPathComponent()
+        url.appendPathComponent(path)
+        guard FileManager.default.fileExists(atPath: url.path) else {
+        fatalError("Unable to find stdlib.")
+        }
+        return url
+    }
+}
