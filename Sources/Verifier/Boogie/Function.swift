60e615d7a8402bc108193d44e18f5bd81b3dbf22
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2019-08-01 13:16:24 +0100

    Fixed and refactored exists and forall Boogie translations (w/Matthew)

--- Sources/Verifier/Boogie/Function.swift
+++ Sources/Verifier/Boogie/Function.swift
@@ -263,14 +263,15 @@ extension BoogieTranslator {
                                    propertyExpression)),
           argumentsStatements + triggerPreStmts,
           argumentPostStmts + triggerPostStmts)
-    case "forall":
+    case "forall", "exists":
+      print(functionCall.arguments)
       assert(argumentsExpressions.count == 3)
       let variableArgument: FunctionArgument = functionCall.arguments[0]
-      let typeArgument: FunctionArgument = functionCall.arguments[0]
+      let typeArgument: FunctionArgument = functionCall.arguments[1]
 
       guard case .identifier(let variable) = variableArgument.expression,
             case .identifier(let type) = typeArgument.expression else {
-        print("forall must be introduced with a typed variable declaration, for some t of type T: `t, T`")
+        print("\(rawFunctionName) must be introduced with a typed variable declaration, for some t of type T: `t, T`")
         fatalError()
       }
       self.currentFunctionReturningValue = variable.name
@@ -283,30 +284,7 @@ extension BoogieTranslator {
       let btype = convertType(
            AST.Type(identifier: AST.Identifier(name: type.name, sourceLocation: typeArgument.sourceLocation)))
 
-      return (.quantified(.forall,
-                          [BParameterDeclaration(name: variable.name, rawName: variable.name, type: btype)],
-                          propertyExpression), [], [])
-    case "exists":
-      assert(argumentsExpressions.count == 3)
-      let variableArgument: FunctionArgument = functionCall.arguments[0]
-      let typeArgument: FunctionArgument = functionCall.arguments[0]
-
-      guard case .identifier(let variable) = variableArgument.expression,
-            case .identifier(let type) = typeArgument.expression else {
-        print("exists must be introduced with a typed variable declaration, for some t of type T: `t, T`")
-        fatalError()
-      }
-      self.currentFunctionReturningValue = variable.name
-      self.currentFunctionReturningValueValue = currentFunctionReturningValue.map { name in
-        return .identifier(name)
-      }
-      let (propertyExpression, _, _) = process(functionCall.arguments[2].expression,
-                                               shadowVariablePrefix: normaliser.getShadowArraySizePrefix)
-
-      let btype = convertType(
-          AST.Type(identifier: AST.Identifier(name: type.name, sourceLocation: typeArgument.sourceLocation)))
-
-      return (.quantified(.exists,
+      return (.quantified((rawFunctionName == "forall" ? .forall : .exists),
                           [BParameterDeclaration(name: variable.name, rawName: variable.name, type: btype)],
                           propertyExpression), [], [])
     default: break
