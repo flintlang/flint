4653de55dc2821206d157710309c4c95e5627818
Matthew Ross Rachar <matthewross@campars.net>
2019-08-07 14:47:16 +0100

    Temporarily revert to dummy error identifier due to stdlib errors

--- Sources/MoveGen/Component/MoveSelf.swift
+++ Sources/MoveGen/Component/MoveSelf.swift
@@ -23,15 +23,7 @@ struct MoveSelf {
       fatalError("Unexpected token \(selfToken.kind)")
     }
     guard !functionContext.isConstructor else {
-      print(#"""
-            \#u{001B}[1;38;5;196mMoveIR generation error:\#u{001B}[0m \#
-            `self' reference before all fields initialized in function `init' in \#(selfToken.sourceLocation)
-
-            \#tCannot use `self' in a constructor before all attributes have been assigned to, \#
-            as some are still unitialized. This includes any method calls which could access instance fields.
-            \#tInstead try moving method calls to after all values have been initialized.
-            """#)
-      exit(1)
+      return .identifier("__ERROR_CONSTRUCTOR_SELF_REFERENCE")
     }
     return .identifier("this")
     // return .identifier(functionContext.isInStructFunction ? "_flintSelf" : (asLValue ? "0" : ""))
